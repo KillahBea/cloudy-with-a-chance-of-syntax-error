ef3bd41a3647ee2d9157c362b7b22fd956450979
KillahBea <bee.noelle6@gmail.com>
2019-05-14 18:44:55 -0400

    it works

--- public/main.js
+++ public/main.js
@@ -1,7 +1,28 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let apiKey = '1bc4af68af30f0969254cdf4fbd7fcec'
+
+// let city-name = 'us'
+
+const getForecastByZip = () => {
+  const id = document.querySelector('input').value
+  fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+      id +
+      '&APPID=01442925e2014b7421bca4fdc5ca55eb&units=imperial'
+  )
+    .then(response => {
+      console.log(response)
+      return response.json()
+    })
+    .then(json => {
+      document.querySelector('.temperature').textContent = json.main.temp
+      console.log(json)
+      // console.log(json.main.temp)
+    })
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const main = () => {
+  console.log('started')
+  getForecastByZip()
+}
+document.querySelector('.get').addEventListener('click', getForecastByZip)
+// document.addEventListener('DOMContentLoaded', main)
